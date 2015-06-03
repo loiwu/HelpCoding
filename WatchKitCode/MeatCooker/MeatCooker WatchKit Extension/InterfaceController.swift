8c5007aa90cf5948d2a82903faa050aa2f12bcfa
Loi Wu <wulei3@ucweb.com>
2015-06-03 17:16:31 +0800

    WatchKit - MeatCooker - Build 0.0.3: Wiring the timer

--- WatchKitCode/MeatCooker/MeatCooker WatchKit Extension/InterfaceController.swift
+++ WatchKitCode/MeatCooker/MeatCooker WatchKit Extension/InterfaceController.swift
@@ -11,6 +11,7 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
+    @IBOutlet weak var timer: WKInterfaceTimer!
 
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
@@ -18,14 +19,12 @@ class InterfaceController: WKInterfaceController {
         // Configure interface objects here.
     }
 
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
-    }
-
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
+    @IBAction func onTimerButton() {
+        println("onTimerButton")
+        let countDown: NSTimeInterval = 20
+        let date = NSDate(timeIntervalSinceNow: countDown)
+        timer.setDate(date)
+        timer.start()
     }
 
 }
