398c038dbd562c1eb47276d8058ff8c4d9250da1
Loi Wu <wulei3@ucweb.com>
2015-06-03 18:01:13 +0800

    SousChef - Starter

---
+++ WatchKitCode/SousChef/SousChef/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SousChef
+//
+//  Created by Loi Wu on 6/3/15.
+//  Copyright (c) 2015 Loi Wu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
