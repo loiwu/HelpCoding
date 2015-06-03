8d0d8793df8ad1f7731043cce19176e195f32cf9
Loi Wu <wulei3@ucweb.com>
2015-06-03 11:15:41 +0800

    WatchKit - HelloAppleWatch - Build 0.0.1

---
+++ WatchKitCode/HelloAppleWatch/HelloAppleWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloAppleWatch
+//
+//  Created by Loi Wu on 6/3/15.
+//  Copyright (c) 2015 Loi Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
