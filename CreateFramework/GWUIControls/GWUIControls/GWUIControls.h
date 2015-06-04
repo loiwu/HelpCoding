fa79ffca6f60f09c19c05255e058d1374ad05031
Loi Wu <wulei3@ucweb.com>
2015-06-04 13:26:22 +0800

    Create the framework project It’s called GWUIControls. It will contain GWKnobControl, and an extra very simple control.

---
+++ CreateFramework/GWUIControls/GWUIControls/GWUIControls.h
@@ -0,0 +1,13 @@
+//
+//  GWUIControls.h
+//  GWUIControls
+//
+//  Created by Loi Wu on 6/4/15.
+//  Copyright (c) 2015 Loi Wu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GWUIControls : NSObject
+
+@end
---
+++ CreateFramework/GWUIControls/GWUIControls/GWUIControls.m
@@ -0,0 +1,13 @@
+//
+//  GWUIControls.m
+//  GWUIControls
+//
+//  Created by Loi Wu on 6/4/15.
+//  Copyright (c) 2015 Loi Wu. All rights reserved.
+//
+
+#import "GWUIControls.h"
+
+@implementation GWUIControls
+
+@end
