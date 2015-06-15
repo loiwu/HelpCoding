dbf55b7245142810ac503896908d0f185d20b524
Loi Wu <wulei3@ucweb.com>
2015-06-15 17:39:04 +0800

    SomeC (Starter)

---
+++ SomeC/SomeC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SomeC
+//
+//  Created by Loi Wu on 6/15/15.
+//  Copyright (c) 2015 Loi Wu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
